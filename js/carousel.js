fae91327bf60e35941d3a45b105f38cce1f47d87
Sanil Shakya <sanilshakya9gmail.com>
2021-05-11 01:19:20 +0545

    query selector corrected, dots array was collecting dots from all carousel instead of just collecting from just its own

--- js/carousel.js
+++ js/carousel.js
@@ -7,7 +7,7 @@ function Carousel(carouselContainer, carouselWidth, transitionTime, holdTime) {
     this.imageWrapper.style.marginLeft = 0;
     this.currentPos = 0;
 
-    
+
     this.init = () => {
         this.assignClassName();
         this.addButton();
@@ -22,12 +22,12 @@ function Carousel(carouselContainer, carouselWidth, transitionTime, holdTime) {
         this.carouselContainer.className = 'carousel-container';
         this.carouselContainer.style.width = carouselWidth;
         this.imageWrapper.className = 'image-wrapper';
-        this.imageList = document.querySelectorAll('.image-wrapper>img');
-        for(let i = 0 ; i < this.imageList.length ; i++){
+        this.imageList = document.querySelectorAll('#' + this.carouselContainer.getAttribute('id') + '>.image-wrapper>img');
+        for (let i = 0; i < this.imageList.length; i++) {
             this.imageList[i].style.width = this.carouselWidth;
         }
     };
-    
+
     this.addButton = () => {
         let prev = document.createElement('div');
         prev.innerText = '‹';
@@ -43,31 +43,29 @@ function Carousel(carouselContainer, carouselWidth, transitionTime, holdTime) {
         let dotsWrapper = document.createElement('div');
         dotsWrapper.className = 'dots-wrapper';
         this.dotsParent = this.carouselContainer.appendChild(dotsWrapper);
-        
-        for(let i = 0; i < this.imageList.length; i++){
+
+        for (let i = 0; i < this.imageList.length; i++) {
             let dots = document.createElement('div');
-        dots.className = 'dots';
+            dots.className = 'dots';
             this.dotsParent.appendChild(dots);
-            console.log(dots);
         }
     }
 
     this.moveImage = (nextPos) => {
-        if(nextPos < 0){
+        if (nextPos < 0) {
             nextPos = this.imageList.length - 1;
         }
-        if(nextPos > this.imageList.length-1){
+        if (nextPos > this.imageList.length - 1) {
             nextPos = 0;
         }
         let finalPos = -nextPos * parseFloat(this.carouselContainer.clientWidth);
         let noOfFrames = (60 / 1000) * transitionTime;
         let speed;
         if (nextPos > this.currentPos) {
-            speed = -(this.carouselContainer.offsetWidth*Math.abs(this.currentPos - nextPos)) / noOfFrames;
+            speed = -(this.carouselContainer.offsetWidth * Math.abs(this.currentPos - nextPos)) / noOfFrames;
         } else {
-            speed = (this.carouselContainer.offsetWidth*Math.abs(this.currentPos - nextPos)) / noOfFrames;
+            speed = (this.carouselContainer.offsetWidth * Math.abs(this.currentPos - nextPos)) / noOfFrames;
         }
-        console.log(nextPos, finalPos, noOfFrames, speed);
         let animation = () => {
             if (speed < 0) {
                 let currentleft = parseFloat(this.imageWrapper.style.marginLeft);
@@ -96,54 +94,55 @@ function Carousel(carouselContainer, carouselWidth, transitionTime, holdTime) {
 
         this.currentPos = nextPos;
         this.dotsArr.forEach((elem, index) => {
-            if(index === this.currentPos){
+            if (index === this.currentPos) {
                 elem.classList.add('active-dot');
-            }else{
+            } else {
                 elem.classList.remove('active-dot');
             }
         })
     }
 
-    this.addBtnEvent = ()=> {
+    this.addBtnEvent = () => {
         this.next.addEventListener('click', () => {
-                this.moveImage(this.currentPos + 1);
+            this.moveImage(this.currentPos + 1);
         })
-    
+
         this.prev.addEventListener('click', () => {
-                this.moveImage(this.currentPos - 1);
+            this.moveImage(this.currentPos - 1);
         })
     }
 
 
     this.addDotsEvent = () => {
-        this.dotsArr = Array.from(document.getElementsByClassName('dots'));
+        this.dotsArr = Array.from(document.querySelectorAll('#' + this.carouselContainer.getAttribute('id') + '>.dots-wrapper>.dots'));
+        console.log(this.dotsArr)
         this.dotsArr.forEach((elem, index) => {
-            elem.addEventListener('click' , () => {
+            elem.addEventListener('click', () => {
                 this.moveImage(index);
             })
         })
 
         this.dotsArr.forEach(elem => {
             let currentDotSize = elem.offsetWidth;
-            elem.addEventListener('mouseover', ()=> {
-                
+            elem.addEventListener('mouseover', () => {
+
                 let animate = () => {
                     let growth = elem.offsetWidth + 1;
                     elem.style.width = growth + 'px';
                     elem.style.height = growth + 'px';
-                    
-                    if(elem.offsetWidth < currentDotSize*1.3){
+
+                    if (elem.offsetWidth < currentDotSize * 1.3) {
                         window.requestAnimationFrame(animate);
                     }
                 }
                 animate();
             })
-            elem.addEventListener('mouseout', ()=>{
+            elem.addEventListener('mouseout', () => {
                 let animate = () => {
                     let shrink = elem.offsetWidth - 1;
-                    elem.style.width = shrink +'px';
-                    elem.style.height = shrink +'px';
-                    if(elem.offsetWidth > currentDotSize){
+                    elem.style.width = shrink + 'px';
+                    elem.style.height = shrink + 'px';
+                    if (elem.offsetWidth > currentDotSize) {
                         window.requestAnimationFrame(animate);
                     }
                 }
@@ -153,10 +152,9 @@ function Carousel(carouselContainer, carouselWidth, transitionTime, holdTime) {
     }
 
     this.autoSlide = () => {
-        let interval = setInterval(()=>{
-            console.log(this.holdTime)
-            this.moveImage(this.currentPos+1);
-        },this.holdTime);
+        let interval = setInterval(() => {
+            this.moveImage(this.currentPos + 1);
+        }, this.holdTime);
     }
 
     this.init();
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const carouselContainer = document.getElementsByClassName('carousel-container');
-const carousel1 = new Carousel(carouselContainer[0], '43vw' ,1000, 7000)
+const carouselContainer = document.getElementById('carousel-container');
+const carouselContainer2 = document.getElementById('carousel-container-2');
+const carousel1 = new Carousel(carouselContainer, '43vw' ,1000, 7000)
+const carousel2 = new Carousel(carouselContainer2, '43vw' ,500, 3000)
 
 
